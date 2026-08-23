733435af9347b2f569c4bf3f973b84407f4f8e22
Ghost9841 <asubedi778@gmail.com>
2026-08-23 20:00:53 +0545

    types of heros and step 0 is done

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 const App = () => {
   return (
     <div>
-      
+      <h1>Self Quest</h1>
+      <p>TBD</p>
     </div>
   )
 }
---
+++ src/types/hero.types.ts
@@ -0,0 +1,9 @@
+export interface Hero {
+  id: string;
+  name: string;
+  avatar: string;
+  totalXP: number;
+  level: number;
+  createdAt: string;
+  updatedAt: string;
+}
